c7efcde6375f01518c79fcfdfc56c520da31fad0
Peter Andreus <peter.andreus@gmail.com>
2017-02-04 22:42:28 +0100

    Sleep cmd
    Updated to add sleep command as i started to use it often

--- pioneerController.sh
+++ pioneerController.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=192.168.100.106
+IP=192.168.1.106
 PORT=23
 
 sendCommand(){
@@ -34,11 +34,36 @@ setPC(){
 	sendCommand '23fn'
 }
 
+setSAT(){
+	sendCommand '06fn'
+}
+
+sleepCmd(){
+	sendCommand "$1sab"
+}
+
 manualCommand(){
 	clear
 	echo -n "Enter command: "
 	read CMD
 	sendCommand "$CMD"
+}
+
+sleepFunc(){
+clear
+echo "1: 30 Min			2: 60 Min"
+echo "3: 90 Min			0: Off"
+
+echo -n "Press any key to continue: "
+read -n1 CMD
+
+case $CMD in
+	1) sleepCmd 030 ;;
+	2) sleepCmd 060 ;;
+	3) sleepCmd 090 ;;
+	0) sleepCmd 000 ;;
+esac
+mainFunction
 }
 
 customVolume(){
@@ -69,8 +94,9 @@ echo " /_/   /_/\\____/_/ /_/\\___/\\___/_/     "
 echo "L: Volume Up			K: Volume Down"
 echo "V: Volume 			M: Mute"
 echo "I: Power On			O: Power Down"
+echo "S: Sleep"
 echo ""
-echo "1: BD	2: PC"
+echo "1: PS4	2: PC	3: SAT"
 echo ""
 echo "W: Manual Command"
 
@@ -83,11 +109,13 @@ case $CMD in
 	[oO]) powerOff ;;
 	[iI]) powerOn ;;
 	[mM]) mute ;;
+	[sS]) sleepFunc ;;
 	[qQ]) clear; exit;;
 	[vV]) customVolume ;;
 	[wW]) manualCommand ;;
 	1) setBD ;;
 	2) setPC ;;
+	3) setSAT ;;
 esac
 mainFunction
 }
